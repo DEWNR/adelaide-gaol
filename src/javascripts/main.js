1a93c74fcec3141c48a115b497a1fc61d10e2a9f
Ben Kannenberg <bkannenberg@gmail.com>
2017-02-07 15:49:59 +1030

    Tripadvisor goes in main.js

---
+++ src/javascripts/main.js
@@ -0,0 +1,7 @@
+
+$(document).ready( function() {
+
+    // trip advisor badge
+    $('.site-header__banner').append( '<div class="site-header__badge"><img alt="trip advisor certificate of excellence" class="site-header__badge--image" src="images/certificate-of-excellence.png"></div>' );
+
+});
